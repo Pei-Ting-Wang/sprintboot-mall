84291c91512c3a7477333c4ddcc8a3cb8c4c5b6e
penny <peiting0210@gmail.com>
2023-07-18 19:35:18 +0800

    read productlist

--- shoppingmall/src/main/java/com/penny/shoppingmall/dao/ProductDao.java
+++ shoppingmall/src/main/java/com/penny/shoppingmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.penny.shoppingmall.dao;
 import com.penny.shoppingmall.model.Product;
 import dto.ProductRequest;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- shoppingmall/src/main/java/com/penny/shoppingmall/service/ProductService.java
+++ shoppingmall/src/main/java/com/penny/shoppingmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.penny.shoppingmall.service;
 import com.penny.shoppingmall.model.Product;
 import dto.ProductRequest;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
