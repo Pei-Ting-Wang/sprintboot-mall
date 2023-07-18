46a76f6f348ccc1034651fbf30dffbc1c752b3b4
penny <peiting0210@gmail.com>
2023-07-18 19:35:18 +0800

    create product

--- shoppingmall/src/main/java/com/penny/shoppingmall/controller/ProductController.java
+++ shoppingmall/src/main/java/com/penny/shoppingmall/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.penny.shoppingmall.model.Product;
 import com.penny.shoppingmall.service.ProductService;
 import dto.ProductRequest;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,12 +28,35 @@ public class ProductController {
         }
     }
 
-    @PostMapping("/prodcuts")
+    @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest){
 
         Integer productId=productService.createProduct(productRequest);
         Product product=productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        Product product=productService.getProductById(productId);
+
+        if(product==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        productService.updateProduct(productId,productRequest);
+        Product updateProduct=productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Integer productId){
+        productService.deleteProduct(productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
